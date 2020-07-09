dad271659e9af3ae4e5f90ba2e81c936e9d46b15
Alexander horat1us Letnikow <reclamme@gmail.com>
2020-07-09 12:37:02 +0300

    Implement support for `examples` in JsonSchema (using Model\AttributesExamples interface)

--- src/Model/AttributesExamples.php
+++ src/Model/AttributesExamples.php
@@ -22,13 +22,13 @@ interface AttributesExamples
      * ```
      *
      * @see https://json-schema.org/understanding-json-schema/reference/generic.html?highlight=examples
-     * @return array<string, array<int, string>|null>
+     * @return array<string, array<int, mixed>|null>
      */
     public function attributesExamples(): array;
 
     /**
      * Returns the examples for the specified attribute.
-     * @return array<int, string>|null
+     * @return array<int, mixed>|null
      */
     public function getAttributeExamples(string $attribute): ?array;
 
