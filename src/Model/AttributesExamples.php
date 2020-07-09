223c1c1d3939b3aa7e86022523c2c95bcbb85f65
Alexander horat1us Letnikow <reclamme@gmail.com>
2020-07-09 11:08:19 +0300

    Add docs into ModelAttributesExamples interface

--- src/Model/AttributesExamples.php
+++ src/Model/AttributesExamples.php
@@ -2,11 +2,37 @@
 
 namespace Horat1us\Yii\Model;
 
+/**
+ * Interface to extend \yii\base\Model with attribute example.
+ * It will be used when Json Schema will be generated.
+ * @since 1.1.0
+ * @author Alexander Letnikow <reclamme@gmail.com>
+ */
 interface AttributesExamples
 {
+    /**
+     * Returns attributes examples.
+     * Each entry should validate against validation rules.
+     *
+     * ```php
+     * return [
+     *     'attributeName' => ['validValue', 'anotherValidValue']
+     * ];
+     * ```
+     *
+     * @see https://json-schema.org/understanding-json-schema/reference/generic.html?highlight=examples
+     * @return string[][]
+     */
     public function attributesExamples(): array;
 
+    /**
+     * Returns the examples for the specified attribute.
+     * @return string[]|null
+     */
     public function getAttributeExamples(string $attribute): ?array;
 
+    /**
+     * Returns first available example for the specified attribute.
+     */
     public function getAttributeExample(string $attribute): ?string;
 }
